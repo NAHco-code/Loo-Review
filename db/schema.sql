82aecfd506e76097ca6f06a84c73392d9b8b18de
Brandon King <brandonscottking@gmail.com>
2021-04-29 18:08:12 -0400

    added name & email to User model; should've looked at the seed data...

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,14 @@
 DROP DATABASE IF EXISTS porcelain_throne;
 CREATE DATABASE porcelain_throne;
+
+
+/*
+user
+    id, username, password;
+locations
+    id, name, loos[]
+loos
+    id, name, reviews[]
+reviews
+    id, description, rating, 
+*/
