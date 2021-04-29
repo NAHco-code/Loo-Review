9e7a553803aac4673c2cbf9f0f11d892f5884ee4
Janeé Mays <janee.elise@gmail.com>
2021-04-29 19:56:10 -0400

    added looReviews.js file

--- models/index.js
+++ models/index.js
@@ -1,18 +1,29 @@
 // IN PROGRESS
-
+// import models
 const User = require('./user');
 const Location = require('./location');
 const Loo = require('./loo');
 
+// 
 Location.hasMany(Loo, {
     foreignKey: 'location_id',
     onDelete: 'CASCADE'
 });
 
+// Bathrooms belongsTo many locations
 Loo.belongsTo(Location, {
+    through: {
+
     foreignKey: 'location_id'
+    },
+as: 
 });
 
 
 
-module.exports = { Location, Loo, User };
+
+module.exports = { 
+    Location, 
+    Loo, 
+    User 
+};
