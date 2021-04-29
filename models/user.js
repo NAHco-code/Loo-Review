4cb74c786560352dddcc48e4cd6e9f07f56d04b4
Brandon King <brandonscottking@gmail.com>
2021-04-29 17:48:36 -0400

    user model

--- models/user.js
+++ models/user.js
@@ -1,23 +1,52 @@
-// IN PROGRESS
-
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model { }
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
         id: {
-
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            }
         }
-
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUser) => {
+                newUser.password = await bcrypt.hash(user.password, 10)
+                return newUser;
+            },
+            beforeUpdate: async (updateUser) => {
+                updateUser.password = await bcrypt.hash(updateUser.password, 10);
+                return updateUser;
+            }
+        }
     },
     {
         sequelize,
         timestamps: true,
         freezeTableName: true,
         underscored: true,
-        modelName: 'loo',
+        modelName: 'user',
     }
 );
 
