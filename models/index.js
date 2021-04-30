92ea460965e2be3f3f17d1d18f9e776476565ec7
Janeé Mays <janee.elise@gmail.com>
2021-04-29 21:47:43 -0400

    models index.js

--- models/index.js
+++ models/index.js
@@ -5,42 +5,42 @@ const Location = require('./location');
 const Loo = require('./loo');
 const looReviews = require('./looReviews');
 
-// 
+// Each location hasMany loos
 Location.hasMany(Loo, {
     foreignKey: 'location_id',
     onDelete: 'CASCADE'
 });
 
-// Bathrooms belongsTo many locations
-Loo.belongsTo(Location, {
-    through: {
-
+// Loos belongsTo many locations
+Loo.belongsTo(Location, { 
     foreignKey: 'location_id'
-    },
-as: 
 });
 
-// User belongsTomany reviews
-User.belongsToMany( looReviews, {
+// User belongsTomany reviews (through looReviews)
+User.belongsToMany(looReviews, {
     through: {
-        model: 
+        model: looReviews,
         unique: false,
     },
-    as: ''
-)
+    as: 'username'
+});
 
 
 // Each Review belongsTo only one user
-
+looReviews.belongsTo(User, { 
+    foreignKey: 'username'
+});
 
 // Loo hasMany reviews
+Loo.hasMany(looReviews, {
+    foreignKey:'loo_id'
+});
 
 
 
-
-module.exports = { 
-    Location, 
-    Loo, 
+module.exports = {
+    Location,
+    Loo,
     User,
-    looReviews, 
+    looReviews,
 };
