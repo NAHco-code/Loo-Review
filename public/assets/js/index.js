0e180fc46b6c93b07f5f97d4c25554dbc542c702
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-30 02:54:10 -0500

    Add public folder, files, + pseudocode

---
+++ public/assets/js/index.js
@@ -0,0 +1,18 @@
+/* Concept:
+        Login + Create Account is taken care of in backend and css
+        search bar
+        main menu
+        rating - figure out how to gain rating functionality // pick up issue: {  } //
+        trip boards // save for last
+        event listeners for each button // pick up issue: {  } //
+        probably more stuff
+
+        ** can or should we break this up into seperate script files? */
+
+// TODO: Declare golbal variables
+
+
+
+// Function populating locations, ...etc.
+
+// Event Listeners
