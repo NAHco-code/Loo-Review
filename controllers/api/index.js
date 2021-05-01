f882e2b96c6885c482ef9bdf7ac2d345417bef7c
Brandon King <brandonscottking@gmail.com>
2021-05-01 13:13:08 -0400

    server starts: issue was empty api routes; api/users/login working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,9 +7,9 @@ const reviewRoutes = require('./reviewRoutes');
 const userRoutes = require('./userRoutes');
 const locationRoutes = require('./locationRoutes');
 
-router.use('/loos', looRoutes);
-router.use('/reviews', reviewRoutes);
+//router.use('/loos', looRoutes);
+//router.use('/reviews', reviewRoutes);
 router.use('/users', userRoutes);
-router.use('/locations', locationRoutes);
+//router.use('/locations', locationRoutes);
 
 module.exports = router;
