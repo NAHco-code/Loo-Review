8377ad293ced74bdeb1e7a902f59afaa195a1c52
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-03 15:54:41 -0500

    Refactor models

--- models/location.js
+++ models/location.js
@@ -1,60 +1,60 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Location extends Model { }
+// class Location extends Model { }
 
-// create fields/columns for Location table
-Location.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        facility_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        address: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
+// // create fields/columns for Location table
+// Location.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true
+//         },
+//         facility_name: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         },
+//         address: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         }
 
-        // **IF NEEDED** // depends on data from google api
+//         // **IF NEEDED** // depends on data from google api
 
-        // facility_name: {
-        //     type: DataTypes.STRING,
-        //     allowNull: false
-        // },
-        // street_number: {
-        //     type: DataTypes.INTEGER,
-        //     allowNull: false
-        // },
-        // street_name: {
-        //     type: DataTypes.STRING,
-        //     allowNull: false
-        // },
-        // city: {
-        //     type: DataTypes.STRING,
-        //     allowNull: false
-        // },
-        // state: {
-        //     type: DataTypes.STRING,
-        //     allowNull: false
-        // },
-        // zip: {
-        //     type: DataTypes.INTEGER,
-        //     allowNull: false
-        // }
-    },
-    {
-        sequelize,
-        timestamps: true,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'location'
-    }
-);
+//         // facility_name: {
+//         //     type: DataTypes.STRING,
+//         //     allowNull: false
+//         // },
+//         // street_number: {
+//         //     type: DataTypes.INTEGER,
+//         //     allowNull: false
+//         // },
+//         // street_name: {
+//         //     type: DataTypes.STRING,
+//         //     allowNull: false
+//         // },
+//         // city: {
+//         //     type: DataTypes.STRING,
+//         //     allowNull: false
+//         // },
+//         // state: {
+//         //     type: DataTypes.STRING,
+//         //     allowNull: false
+//         // },
+//         // zip: {
+//         //     type: DataTypes.INTEGER,
+//         //     allowNull: false
+//         // }
+//     },
+//     {
+//         sequelize,
+//         timestamps: true,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'location'
+//     }
+// );
 
-module.exports = Location;
+// module.exports = Location;
