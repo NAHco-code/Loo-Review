f9afebc5ad3f2c50da2ed23e8d3240d25a4dc763
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-04 02:23:44 -0500

    Update code comments

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 const { Location, User, Review } = require('../../models')
 
+// The `/api/reviews` endpoint
+
 module.exports = router;
--- models/loo.js
+++ models/loo.js
@@ -19,6 +19,18 @@ Loo.init(
         address: { // format still in question  - string or obeject
             type: DataTypes.STRING,
             allowNull: false
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        zip: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         }
     },  // render average rating with query
         // individual ratings from each review in review table
