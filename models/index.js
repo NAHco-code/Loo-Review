3e5563c63bb9858d5e562b826e9d001e3696e7b5
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-03 23:35:22 -0500

    Try to get 'GET' routes working

--- models/index.js
+++ models/index.js
@@ -9,10 +9,11 @@
 
 // #### LOO TABLE
 // * ID(PK)
+// * Review ID
+// * User ID
 // * Facility name
 // * Address (string or object - TBD)
 // *    // if object, may need: street_number, street_name, city, state, zip
-// * Rating (avg rating - sequelize literal - update loo after each review - can do in front end - doesn't need to be in table)
 
 // #### REVIEW TABLE
 // * ID(PK)
@@ -34,23 +35,13 @@ const Review = require('./Review');
 // ex. A.hasOne(B); // 'A' is the source model // 'B' is the target model
 // https://sequelize.org/master/manual/assocs.html
 
-// location seems the starting point - that's how the user finds the loos, which hold the reviews
 
-
-// One-To-Many relationship // *created by using '.hasMany' + '.belongsTo' associations together
-Loo.hasMany(Review, { onDelete: 'CASCADE', foreignKey: 'loo_id' });// .hasMany association - fk is defined in target model
-Review.belongsTo(Loo, { foreignKey: 'loo_id' });// .belongsTo association - fk is defined in source model
 
 User.hasMany(Review, { foreignKey: 'user_id' });
 Review.belongsTo(User, { foreignKey: 'user_id' });
 
-// many to many relationship between users and loos - render username on who added a loo
-User.belongsToMany(Loo, { foreignKey: 'user_id' });
-Loo.belongsToMany(User, { foreignKey: 'loo_id' });
-
-// relationship b/w user and loo if we want to save favorites //*after MVP is reached
-
-
+Loo.hasMany(User, { include: Review, foreignKey: 'loo_id' });
+User.belongsTo(Loo, { include: Review, foreignKey: 'loo_id' });
 
 
 module.exports = {
