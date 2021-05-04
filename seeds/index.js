0d811ac61b5dcb1f6f19553be1b607adf5a675aa
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-03 23:31:41 -0500

    Refactor + add seeds data

--- seeds/index.js
+++ seeds/index.js
@@ -1,28 +1,24 @@
-// IN PROGRESS // following code from class example
+// IN PROGRESS // following code from E-Commerce hw (now that json files were imported to js files)
+
+const seedUsers = require('./userData');
+const seedLoos = require('./looData');
+const seedReviews = require('./reviewData');
+// const seedLocations = require('./locationData.json');
 
 const sequelize = require('../config/connection');
-const { User, Loo, Review } = require('../models');
-
-const userData = require('./userData');
-// const locationData = require('./locationData.json');
-const looData = require('./looData');
-const reviewData = require('./reviewData');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
 
-    const loo = await Loo.bulkCreate(looData, {
-        individualHooks: true,
-        returning: true,
+    await seedUsers();
+    console.log('\n----- USERS SYNCED -----\n');
 
-    });
+    await seedLoos();
+    console.log('\n----- LOOS SYNCED -----\n');
 
-    for (const review of reviewData) {
-        await Review.create({
-            ...review,
-            loo_id: loo //math floor random * users.legth)].id,
-        });
-    }
+    await seedReviews();
+    console.log('\n----- REVIEWS SYNCED -----\n');
 
     process.exit(0);
 };
