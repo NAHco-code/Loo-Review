848ee0049d326c2015b79bc2c15841505357ab31
Brandon King <brandonscottking@gmail.com>
2021-05-04 20:58:48 -0400

    handlebar updates, frontend js updates

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -12,32 +12,35 @@
 
 // TODO: Declare golbal variables
 // haversine calculation - if you have coordinates it filters the locations
-const locations = [
-        {lat: 39.9902047, lng: -83.0061},
-        {lat: 39.9892452, lng: -83.0054187},
-        {lat: 39.9939083, lng: -83.0062601},
-        {lat: 39.994207, lng: -83.0059488}
-    ]
 
-    function initMap() {
-        const myLatLng = locations[0];
-        const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 16,
-          center: myLatLng,
-        });
-        for (place of locations){
-            new google.maps.Marker({
-            position: place,
-            map,
-            title: "NAME",
-            });
-        }
-      };
+const locations = [
+  { lat: 39.9902047, lng: -83.0061 },
+  { lat: 39.9892452, lng: -83.0054187 },
+  { lat: 39.9939083, lng: -83.0062601 },
+  { lat: 39.994207, lng: -83.0059488 }
+]
+
+const initMap = async () => {
+  const myLatLng = await window.navigator.geolocation.getCurrentPosition(((locale) => {
+    return { lat: locale.coords.latitude, lng: locale.coords.longitude };
+  }, () => { alert('This application will not function properly without your location') }));
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: myLatLng,
+  });
+  for (place of locations) {
+    new google.maps.Marker({
+      position: place,
+      map,
+      title: "NAME",
+    });
+  }
+};
 
 // Function populating locations, ...etc.
 
 //Event Listeners
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
 });
