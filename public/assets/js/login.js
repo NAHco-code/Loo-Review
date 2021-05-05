694e2360ba2b531198d7f85666d6b648bcc830e1
Brandon King <brandonscottking@gmail.com>
2021-05-04 21:22:39 -0400

    merging from main

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,7 +1,31 @@
-const createBtn = document.getElementById('create');
-const loginBtn = document.getElementById('login');
+//dom elements
+const createBtn = document.querySelector('#create');
+const loginBtn = document.querySelector('#login');
+//user input fields
+const createUserName = document.querySelector('#username-a');
+const createPwd = document.querySelector('#password-a');
+const userEmail = document.querySelector('#email');
+const  usersName = document.querySelector('#name');
+const logUserName = document.querySelector('username-b');
+const logPwd = document.querySelector('#password-b');
+
+
+loginBtn.addEventListener('click', (event)=> {
+    event.preventDefault();
+    const loginObj = {
+        username: logUserName.value,
+        password: logPwd.value
+    };
+
+});
 
 createBtn.addEventListener('click', (event)=> {
     event.preventDefault();
-    
-})
+    const newUserObj = {
+        name: usersName.value,
+        username: createUserName.value,
+        email: userEmail.value,
+        password: createPwd.value
+    }
+});
+
