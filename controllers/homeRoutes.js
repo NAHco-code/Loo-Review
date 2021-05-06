0cc9ad3ac438c8164151b3f23392d8414f4f44ef
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-05 20:28:30 -0500

    I hate routing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,56 +1,80 @@
-// From the homepage - we want to load nearby loos and map
-
-
-// COMPLETE + TESTED
-    // get ('/') homepage renders filtered loo data and map
-    // get ('/loo/:id') route working - page not yet built
-    // get ('/login')
-
-// *reference homeRoutes in MVPunit student mini proj + ECommerce hs
 
 const router = require('express').Router();
-const { Loo, User, Review, UsersLoos } = require('../models'); //reads index.js
-const withAuth = require('../utils/auth'); //fix password encryption & authentication
+const sequelize = require('../config/connection')
+const { Loo, User, Review, UsersLoos } = require('../models');
+// const { QueryTypes } = require('sequelize');
+const withAuth = require('../utils/auth'); // authorization middleware
+const haversine = require('s-haversine');
+console.log(haversine);
 
-// TODO: homepageRoute renders nearby loos (through location)
-// GET all loos
-router.get('/', async (req, res) => { // WORKING
-    try {
-        // TODO: **What data are we using/are we able to use @here?**
-        /* createFind? which method to use? */
-        const looData = await Loo.findAll({ include: [ Review ] });
-
-        //TODO: haversine function
-
-        // Serialize data so the template can read it
-        const loos = looData.map((loos) => loos.get({ plain: true }));
+// *get render login
+// *get render filtered loos
+// get map??
+// *get loo by id
 
 
-        // Pass serialized data and session flag to template
-        // res.render('homepage', {
-        //     //render map //make data call to render loos and populate map
-        //     logged_in: req.session.logged_in
-        // });
-        res.status(200).json(looData); //for testing
+//LOGIN PAGE //*redirect to main page once logged in *** WORKING
+router.get('/', (req, res) => {
 
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err);
+    if (req.session.logged_in) {// If the user is already logged in,
+        res.redirect('/main'); // redirect to main page
+        return;
     }
+
+    res.render('login');
 });
 
-// TODO: /loo/:id renders a specific loo + associated reviews
-router.get('/loo/:id', async (req, res) => { //WORKING
+// ** get new user page? - same as log in page - post route is different - why? **
+
+
+// MAIN PAGE //*render filtered loos (( + map?? )) *** WORKING
+router.get('/main', async (req, res) => {
+
     try {
-        const looData = await Loo.findByPk(req.params.id, { include: [ Review ] });
+        const looData = await Loo.findAll({ include: [Review] });
 
-        const loo = looData.get({ plain: true });
+        const loos = looData.map((loos) => loos.get({ plain: true }));
 
-        // res.render('selected_loo+reviews', {
-        //     loo,
-        //     logged_in: req.session.logged_in
-        // });
-        res.status(200).json(looData); //for testing
+        // filter loos (based on user location) using haversine npm package
+        const filteredLoos = loos.filter((loo) => { // filter expects a condition
+
+            console.log(req.query);
+            console.log( )
+            return haversine.default.distance([Number(req.query.lat), Number(req.query.lon)], [loo.lat, loo.lon]) < 1609.34 * 10; // Meter to mile conversion // 10 mile radius
+        })
+        res.render('homepage', {
+            //render map //make data call to render loos and populate map
+            loos,
+            logged_in: req.session.logged_in
+        });
+        console.log(filteredLoos);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
+    // NOTES FOR FRONT END
+// /api/loos?lat=33.333&lon=-123 *URL *lat1 lng1
+// when user adds loo - must take address and feed it into geolocation and get lat + long
+// will have to update the map with the rendered loo markers
+})
+
+
+// VIEW LOO PAGE
+router.get('/loo/:id', async (req, res) => { // *** WORKING
+// TODO: /loo/:id renders a specific loo + associated reviews
+    try {
+        const looData = await Loo.findByPk(req.params.id, { include: [Review] });
+        // get review data, include loo + user [attribute: name, createdAt]
+
+        const loo = looData.get({ plain: true });// Serialize data so the template can read it
+
+        res.render('view-loo', {
+            loo,
+            logged_in: req.session.logged_in
+        });
+        //res.status(200).json(looData); //for testing
+
 
     } catch (err) {
         res.status(500).json(err);
@@ -58,43 +82,9 @@ router.get('/loo/:id', async (req, res) => { //WORKING
     }
 });
 
-// Use withAuth middleware to prevent access to route
-// router.get('/review', withAuth, async (req, res) => {
-//     try {
-//         // Find the logged in user based on the session ID
-//         const userData = await User.findByPk(req.session.user_id, {
-//             attributes: { exclude: ['password'] },
-//             include: [{ model: review }],
-//         });
-
-//         const user = userData.get({ plain: true });
-
-//         res.render('review', {
-//             ...user,
-//             logged_in: true
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-
-//      *** once review is submitted, render loo page with
-//            reviewed loo selected
-// });
-
-
-// TODO: how to write the route for adding a new loo
-
-
-
-router.get('/login', (req, res) => { //WORKING
-  // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-    }
-
-    res.render('login');
-});
-
-
 module.exports = router;
+// / (api endpoint)
+
+// / (log in)
+// /main (homepage)
+// /loo/:id (selected_loo+reviews)
