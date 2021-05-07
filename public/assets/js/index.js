9eabf6496fffb556be66a6ea9ebb488e01af01aa
Brandon King <brandonscottking@gmail.com>
2021-05-07 10:16:59 -0400

    added rating field to review form; not currently working

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -103,6 +103,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Modal.init(elems, {});
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, options);
+});
 
 // document.addEventListener('DOMContentLoaded', fetchUserLocation() {
 //   //return filteredLoos variable *defined in homeRoutes
